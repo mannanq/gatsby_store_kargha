a379486240b483b4f1cbc61247f4416cc945b2cc
Mannan Qazi <mannanqazi2@gmail.com>
2019-05-23 14:50:18 -0400

    connected to contentful

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,10 +5,12 @@ import PropTypes from "prop-types"
 import "./bootstrap.min.css"
 import "./layout.css"
 import Navbar from "./Globals/Navbar"
+import Footer from "./Globals/Footer"
 const Layout = ({ children }) => (
   <>
     <Navbar />
     {children}
+    <Footer />
   </>
 )
 
