6f9c143bf66ad856b5c0a1f4b264ce8c3ea8edba
Mannan Qazi <mannanqazi2@gmail.com>
2019-05-23 20:49:30 -0400

    added for sale products, contact form with form-spree

---
+++ src/components/Home/Contact.js
@@ -0,0 +1,63 @@
+import React from "react"
+import Title from "../Globals/Title"
+
+export default function Contact() {
+  return (
+    <section className="contact py-5">
+      <Title title="contact us" />
+      <div className="row">
+        <div className="col-10 col-sm-8 col-md-6 mx-auto">
+          <form action="https://formspree.io/i@mannanqz.com" method="POST">
+            {/* name */}
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                className="form-control"
+                name="name"
+                id="name"
+                placeholder="john smith"
+              />
+            </div>
+            {/* email */}
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                name="_replyto"
+                placeholder="Your email"
+                className="form-control"
+              />
+
+              {/* <input
+                type="text"
+                className="form-control"
+                name="email"
+                id="email"
+                placeholder="johnsmith@email.com"
+              /> */}
+            </div>
+            {/* message */}
+            <div className="form-group">
+              <label htmlFor="message">Message</label>
+              <textarea
+                name="message"
+                id="message"
+                className="form-control"
+                rows="5"
+                placeholder="Your message here..."
+              />
+            </div>
+            {/* submit button */}
+            <button
+              type="submit"
+              className="btn btn-yellow btn-block text-capitalize mt-5"
+            >
+              Submit Message
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  )
+}
